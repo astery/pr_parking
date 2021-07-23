40028b6dadccfd3b6300817fcd6cb7688f2bdab5
Andrey Akulov <andreyshark@gmail.com>
2021-07-23 06:19:23 +0300

    docs: Add commentaries about missing part

--- apps/pr_parking/lib/pr_parking.ex
+++ apps/pr_parking/lib/pr_parking.ex
@@ -29,6 +29,10 @@ defmodule PrParking do
     @behaviour Behaviour
 
     def get_pr_parking(_id) do
+      # Here should be calls to an api wrapped in cache layer
+      #
+      # Cached.call({MojeprahaApi, :get_pr_parking, [id]})
+
       {:ok,
        %{
          num_of_taken_places: 10,
@@ -37,12 +41,27 @@ defmodule PrParking do
     end
 
     def set_pr_parking_refresh_period(_id, _period) do
+      # Here should be calls to cache layer config
+      #
+      # Cached.set_refresh_period({MojeprahaApi, :get_pr_parking, [id]}, period)
+
       :ok
     end
   end
 
   @behaviour Behaviour
   @adapter Application.compile_env(:pr_parking, :module, Impl)
+
+  # Here should be functions to start process (deletating to Cached)
+  #
+  # def start_link(opts) do
+  #   cached_opts =
+  #     Applications.get_env(:pr_parking, :resources)
+  #     |> Enum.map(...)
+  #
+  #   ...
+  #   Cached.start_link(cached_opts)
+  # end
 
   defdelegate get_pr_parking(id), to: @adapter
   defdelegate set_pr_parking_refresh_period(id, period), to: @adapter
